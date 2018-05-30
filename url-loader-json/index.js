1a34e395a2dbe91957dd3a8e006f45ad97f84b46
jose.ocana <jot74343>
2018-05-30 17:55:45 +0200

    añadido nuevo plugin para cargar json: json-loader

---
+++ url-loader-json/index.js
@@ -0,0 +1,26 @@
+import './estilos.css'
+import { firstMessaje } from './messaje.js'
+import atpPremium from './atp-premium.jpg'
+import video2mb from './video/SampleVideo_1280x720_2mb.mp4'
+import data from './json/teachers.json';
+
+console.log(data);
+
+data.teachers.forEach((teacher) => {
+  const element = document.createElement('p');
+  element.textContent = teacher.name;
+  document.body.appendChild(element);
+})
+
+document.write(firstMessaje);
+const img = document.createElement('img');
+img.setAttribute('src', atpPremium);
+document.body.appendChild(img);
+const video = document.createElement('video');
+video.setAttribute('src', video2mb);
+video.setAttribute('autoplay', true);
+video.setAttribute('controls', true);
+video.setAttribute('width', '480');
+
+document.body.appendChild(video);
+console.log("Hola Mundo desde la carpeta css-style-loader");
