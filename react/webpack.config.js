6075b23b7127ffa0cce302d592c60f912b3bc350
jose.ocana <jot74343>
2018-05-31 11:22:58 +0200

    añadido react a webpack y creados componentes de prueba

---
+++ react/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js',
+    publicPath: './dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.json$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+           use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(jpg|png|gif|woff)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            limit: 100000,
+            name: 'videos/[name].[hash].[ext]'
+          }
+        }
+      }
+    ]
+  }, 
+  plugins: [
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
