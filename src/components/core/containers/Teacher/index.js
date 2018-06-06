a5f61570ba4c0d4b82d469330209376ce36d894e
jose.ocana <jot74343>
2018-06-06 10:57:47 +0200

    Iniciado React en el proyecto

---
+++ src/components/core/containers/Teacher/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Teacher(props){
+  return (
+    <li>Nombre: {props.name}, Twitter: {props.twitter}</li>
+  )
+}
+
+export default Teacher 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hola mundo del webpack. Actualizado!!!!");
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Teacher from './components/core/containers/Teacher/'
+
+console.log("Iniciando app...");
+
+render(<Teacher name="Jose" twitter="jmocana2" />, document.getElementById('app'));
+
