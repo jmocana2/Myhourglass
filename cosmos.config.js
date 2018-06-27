37e7210203837e7f7638e665aa9af240c4697554
jose.ocana <jot74343>
2018-06-27 11:29:47 +0200

    Ajustes de configuración para el webpack de cosmos

--- cosmos.config.js
+++ cosmos.config.js
@@ -1,6 +1,6 @@
 // cosmos.config.js
 module.exports = {
   containerQuerySelector: '#root',
-  webpackConfigPath: 'react-scripts/config/webpack.config.dev',
+  webpackConfigPath: './webpack.config.cosmos.js',
   publicPath: 'public',
 };
---
+++ webpack.config.cosmos.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'bundle.js'
+  },
+  devServer : {
+    port: 8080,
+    disableHostCheck: true,
+    hot: true,
+    contentBase: './dist/',
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']   
+          }
+        }
+      },    
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+           use: 'css-loader'
+        })
+      },
+      {
+        test: /\.(jpg|png|gif|woff)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000
+          }
+        }
+      },
+      {
+        test: /\.(mp4|webm)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            limit: 100000,
+            name: 'videos/[name].[hash].[ext]'
+          }
+        }
+      }
+    ]
+  }, 
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+
+      title: 'My Hourglass'
+  
+    })   
+  ]
+}
